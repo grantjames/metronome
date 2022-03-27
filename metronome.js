df99df6478ccb6b3cd2df63012f3635f4268298b
Grant <grantjames@live.co.uk>
2022-03-27 23:36:05 +0100

    Add ability to configure number of beats per bar

--- metronome.js
+++ metronome.js
@@ -4,7 +4,8 @@ class Metronome
     {
         this.audioContext = null;
         this.notesInQueue = [];         // notes that have been put into the web audio and may or may not have been played yet {note, time}
-        this.currentQuarterNote = 0;
+        this.currentBeatInBar = 0;
+        this.beatsPerBar = 4;
         this.tempo = tempo;
         this.lookahead = 25;          // How frequently to call scheduling function (in milliseconds)
         this.scheduleAheadTime = 0.1;   // How far ahead to schedule audio (sec)
@@ -19,9 +20,9 @@ class Metronome
         var secondsPerBeat = 60.0 / this.tempo; // Notice this picks up the CURRENT tempo value to calculate beat length.
         this.nextNoteTime += secondsPerBeat; // Add beat length to last beat time
     
-        this.currentQuarterNote++;    // Advance the beat number, wrap to zero
-        if (this.currentQuarterNote == 4) {
-            this.currentQuarterNote = 0;
+        this.currentBeatInBar++;    // Advance the beat number, wrap to zero
+        if (this.currentBeatInBar == this.beatsPerBar) {
+            this.currentBeatInBar = 0;
         }
     }
 
@@ -34,7 +35,7 @@ class Metronome
         const osc = this.audioContext.createOscillator();
         const envelope = this.audioContext.createGain();
         
-        osc.frequency.value = (beatNumber % 4 == 0) ? 1000 : 800;
+        osc.frequency.value = (beatNumber % this.beatsPerBar == 0) ? 1000 : 800;
         envelope.gain.value = 1;
         envelope.gain.exponentialRampToValueAtTime(1, time + 0.001);
         envelope.gain.exponentialRampToValueAtTime(0.001, time + 0.02);
@@ -50,7 +51,7 @@ class Metronome
     {
         // while there are notes that will need to play before the next interval, schedule them and advance the pointer.
         while (this.nextNoteTime < this.audioContext.currentTime + this.scheduleAheadTime ) {
-            this.scheduleNote(this.currentQuarterNote, this.nextNoteTime);
+            this.scheduleNote(this.currentBeatInBar, this.nextNoteTime);
             this.nextNote();
         }
     }
@@ -66,7 +67,7 @@ class Metronome
 
         this.isRunning = true;
 
-        this.currentQuarterNote = 0;
+        this.currentBeatInBar = 0;
         this.nextNoteTime = this.audioContext.currentTime + 0.05;
 
         this.intervalID = setInterval(() => this.scheduler(), this.lookahead);
