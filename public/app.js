22e51ca4e4187133ef35e3c20d88e5bbb59aa38e
Colin McGowan <cjm.online@gmail.com>
2023-09-19 18:17:43 +0100

    Fixed Volume starting value

--- public/app.js
+++ public/app.js
@@ -1,11 +1,11 @@
 var metronome = new Metronome();
 
 
-var bpm = document.getElementById('bpmIndicator');
-var volume = document.getElementById('volumeIndicator');
+var bpmDisplayValue = document.getElementById('bpmIndicator');
+var volumeDisplayValue = document.getElementById('volumeIndicator');
 
-bpm.textContent = metronome.bpm;
-volume.textContent = metronome.volume;
+bpmDisplayValue.textContent = metronome.bpm;
+volumeDisplayValue.textContent = parseInt(metronome.volume * 100) + "%"
 
 var playButton = document.getElementById('pauseplaylogo');
 playButton.addEventListener('click', function() {
@@ -22,20 +22,20 @@ const volumeSlider = document.getElementById('volumeSlider');
 // Add an event listener to the slider for BPM changes
 bpmSlider.addEventListener('input', () => {
     metronome.bpm = parseInt(bpmSlider.value);
-    bpm.textContent = metronome.bpm;
+    bpmDisplayValue.textContent = metronome.bpm;
 });
 
 // Add an event listener to the slider for volume changes
 volumeSlider.addEventListener('input', () => {
     metronome.volume = parseFloat(volumeSlider.value);
-    volume.textContent = parseInt(metronome.volume * 100) + "%";
+    volumeDisplayValue.textContent = parseInt(metronome.volume * 100) + "%";
 });
 
 decrease1.addEventListener('click', function() {
         metronome.bpm -= 1;
-        bpm.textContent = metronome.bpm;
+        bpmDisplayValue.textContent = metronome.bpm;
     });
 increase1.addEventListener('click', function() {
         metronome.bpm += 1;
-        bpm.textContent = metronome.bpm;
+        bpmDisplayValue.textContent = metronome.bpm;
     });
