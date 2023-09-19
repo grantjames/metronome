63c8d199a1839d7707367914ca58a037aa6792f1
Colin McGowan <cjm.online@gmail.com>
2023-09-19 13:34:52 +0100

    Working metronome for navbar. Javascript all working

--- public/app.js
+++ public/app.js
@@ -1,26 +1,32 @@
 var metronome = new Metronome();
-var tempo = document.getElementById('tempo');
-tempo.textContent = metronome.tempo;
+var bpm = document.getElementById('bpmIndicator');
+bpm.textContent = metronome.bpm;
 
-var playPauseIcon = document.getElementById('play-pause-icon');
-
-var playButton = document.getElementById('play-button');
+var playButton = document.getElementById('pauseplaylogo');
 playButton.addEventListener('click', function() {
     metronome.startStop();
-
-    if (metronome.isRunning) {
-        playPauseIcon.className = 'pause';
-    }
-    else {
-        playPauseIcon.className = 'play';
-    }
-
 });
 
-var tempoChangeButtons = document.getElementsByClassName('tempo-change');
-for (var i = 0; i < tempoChangeButtons.length; i++) {
-    tempoChangeButtons[i].addEventListener('click', function() {
-        metronome.tempo += parseInt(this.dataset.change);
-        tempo.textContent = metronome.tempo;
+const decrease5 = document.getElementById('decrease5');
+const decrease1 = document.getElementById('decrease1');
+const increase1 = document.getElementById('increase1');
+const increase5 = document.getElementById('increase5');
+var bpmChangeButtons = document.getElementsByClassName('bpm-change');
+
+decrease5.addEventListener('click', function() {
+        metronome.bpm += -5;
+        bpm.textContent = metronome.bpm;
+    }
+);
+decrease1.addEventListener('click', function() {
+        metronome.bpm += -1;
+        bpm.textContent = metronome.bpm;
     });
-}
+increase1.addEventListener('click', function() {
+        metronome.bpm += 1;
+        bpm.textContent = metronome.bpm;
+    });
+increase1.addEventListener('click', function() {
+        metronome.bpm += 5;
+        bpm.textContent = metronome.bpm;
+    });
